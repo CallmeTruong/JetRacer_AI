a555bf330ad1860fbc816e23a1919a25261ace8c
CallmeTruong <truongpmn@gmail.com>
2026-08-11 15:52:48 +0700

    fix bugs

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
-#JetCar full setup
+#!/bin/bash
 set -e
 
 cd "$HOME"
 
-echo "==== Installing JetCam===="
-if [! -d "$HOME/jetcam"]; then
+echo "==== Installing JetCam ===="
+if [ ! -d "$HOME/jetcam" ]; then
     git clone https://github.com/NVIDIA-AI-IOT/jetcam.git
 fi
 
 cd "$HOME/jetcam"
 sudo python3 setup.py install
 
-echo "==== Installing torch2trt===="
-if [! -d "$HOME/torch2trt"]; then
+
+echo "==== Installing torch2trt ===="
+if [ ! -d "$HOME/torch2trt" ]; then
     git clone https://github.com/NVIDIA-AI-IOT/torch2trt.git
 fi
 
@@ -32,7 +33,5 @@ sudo python3 setup.py install
 echo "=== Setting Jetson Nano to 5W mode ==="
 sudo nvpmodel -m1
 
-
 echo ""
-echo " JetRacer setup completed successfully"
-
+echo "JetRacer setup completed successfully"
